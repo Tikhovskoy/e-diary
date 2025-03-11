b36c90fb3c748dcde3449a314624371f3db988d3
Tikhovskoy <tihovskoyviktor@icloud.com>
2025-03-11 19:01:21 +0300

    Вынесен поиск ученика в get_schoolkid(), код стал чище

--- fix_chastisements.py
+++ fix_chastisements.py
@@ -1,10 +1,16 @@
-from datacenter.models import Schoolkid, Chastisement
+from datacenter.models import Chastisement
+from commendations import get_schoolkid
 
-def remove_chastisements(schoolkid):
-    chastisements = Chastisement.objects.filter(schoolkid=schoolkid)
-    deleted_count, _ = chastisements.delete()
+def remove_chastisements(schoolkid_name):
+    try:
+        schoolkid = get_schoolkid(schoolkid_name)
+        chastisements = Chastisement.objects.filter(schoolkid=schoolkid)
+        deleted_count, _ = chastisements.delete()
 
-    if deleted_count:
-        print(f"Удалено {deleted_count} замечаний у {schoolkid.full_name}!")
-    else:
-        print(f"У {schoolkid.full_name} нет замечаний!")
+        if deleted_count:
+            print(f"Удалено {deleted_count} замечаний у {schoolkid.full_name}!")
+        else:
+            print(f"У {schoolkid.full_name} нет замечаний!")
+
+    except Exception as e:
+        print(f"Ошибка: {e}")
--- fix_marks.py
+++ fix_marks.py
@@ -1,10 +1,16 @@
-from datacenter.models import Schoolkid, Mark
+from datacenter.models import Mark
+from commendations import get_schoolkid
 
-def fix_marks(schoolkid):
-    bad_marks = Mark.objects.filter(schoolkid=schoolkid, points__in=[2, 3])
-    updated_count = bad_marks.update(points=5)
+def fix_marks(schoolkid_name):
+    try:
+        schoolkid = get_schoolkid(schoolkid_name)
+        bad_marks = Mark.objects.filter(schoolkid=schoolkid, points__in=[2, 3])
+        updated_count = bad_marks.update(points=5)
 
-    if updated_count:
-        print(f"Исправлено {updated_count} оценок!")
-    else:
-        print("Нет плохих оценок для исправления!")
+        if updated_count:
+            print(f"Исправлено {updated_count} оценок!")
+        else:
+            print("Нет плохих оценок для исправления!")
+
+    except Exception as e:
+        print(f"Ошибка: {e}")
